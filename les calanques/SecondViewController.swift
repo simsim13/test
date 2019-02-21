c57d532544e51cc5f136df903333ee9ad9f7de23
simsim <simsim@simsims-imac.local>
2019-02-21 09:19:40 -0800

    Initial Commit

---
+++ les calanques/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  les calanques
+//
+//  Created by simsim on 21/02/2019.
+//  Copyright © 2019 simsim. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
